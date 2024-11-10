c3d50b14aa8ef2875d754951afaa0868f8f92122
0Halley0 <haleberins@gmail.com>
2024-11-10 10:56:25 +0300

    Deleted tutorial files

--- forum/src/index.js
+++ forum/src/index.js
@@ -1,26 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-//import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import Router from "./Router";
-import { BrowserRouter } from "react-router-dom";
-//import FunctionComponentExample from "./components/FunctionComponentExample";
-//import ClassComponentExample from "./components/ClassComponentExample";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
-
-    {/*<ClassComponentExample />*/}
-    {/* <FunctionComponentExample /> */}
+    <App />
   </React.StrictMode>
 );
 
