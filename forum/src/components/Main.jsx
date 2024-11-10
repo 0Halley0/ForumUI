e651ff1a959f3166ec2e7d80e8d86f9a061557b8
0Halley0 <haleberins@gmail.com>
2024-11-10 11:32:49 +0300

    Setup tailwind

---
+++ forum/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../main.css"
+
+export default function Main() {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum suscipit
+        laborum aspernatur quae aliquam, eveniet quia nesciunt architecto earum
+        ut dicta cupiditate placeat et nisi, qui sed necessitatibus aperiam
+        error. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis
+        blanditiis nobis culpa veritatis ex harum minima reiciendis. Laborum
+        earum unde quo. Accusamus temporibus maiores est eveniet eum nihil
+        voluptate veritatis.
+      </p>
+    </div>
+  );
+}
