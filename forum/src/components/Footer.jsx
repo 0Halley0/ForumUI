0e220c844af32012dc1b36bdb293cc2d3d837239
0Halley0 <haleberins@gmail.com>
2024-11-19 18:04:26 +0300

    Updated footer design

--- forum/src/components/Footer.jsx
+++ forum/src/components/Footer.jsx
@@ -2,6 +2,44 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className="bg-background dark:bg-dark-background border-t border-black ">
+      {/* Desktop Navigation Buttons*/}
+      <div className="hidden sm:flex justify-center align-middle gap-16 p-4">
+        <button className="text-text dark:text-dark-text hover:text-goldenRod dark:hover:text-gold transition duration-200">
+          About
+        </button>
+        <button className="text-text dark:text-dark-text hover:text-goldenRod dark:hover:text-gold transition duration-200">
+          Membership
+        </button>
+        <button className="text-text dark:text-dark-text hover:text-goldenRod dark:hover:text-gold transition duration-200">
+          Contact
+        </button>
+        <button className="text-text dark:text-dark-text hover:text-goldenRod dark:hover:text-gold transition duration-200">
+          Sign in
+        </button>
+        <button className="text-text dark:text-dark-text hover:text-goldenRod dark:hover:text-gold transition duration-200">
+          Terms and Privacy
+        </button>
+      </div>
+
+      {/* Mobile Navigation Buttons */}
+      <div className="sm:hidden flex flex-col p-4">
+        <button className="text-text dark:text-dark-text hover:text-goldenRod dark:hover:text-gold transition duration-200">
+          About
+        </button>
+        <button className="text-text dark:text-dark-text hover:text-goldenRod dark:hover:text-gold transition duration-200">
+          Membership
+        </button>
+        <button className="text-text dark:text-dark-text hover:text-goldenRod dark:hover:text-gold transition duration-200">
+          Contact
+        </button>
+        <button className="text-text dark:text-dark-text hover:text-goldenRod dark:hover:text-gold transition duration-200">
+          Sign in
+        </button>
+        <button className="text-text dark:text-dark-text hover:text-goldenRod dark:hover:text-gold transition duration-200">
+          Terms and Privacy
+        </button>
+      </div>
+    </div>
   );
 }
